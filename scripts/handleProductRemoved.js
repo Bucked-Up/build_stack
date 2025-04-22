1a99f8085cb4eec8defabafa39e44f3e8c966145
deigo-lps <diego.lopes0909@gmail.com>
2025-04-22 17:15:14 -0300

    fixes removed logic

--- scripts/handleProductRemoved.js
+++ scripts/handleProductRemoved.js
@@ -3,8 +3,11 @@ import handlePrice from "./handlePrice.js";
 const handleProductRemoved = async ({ prod, value }) => {
   const currentProducts = await JSON.parse(localStorage.getItem("stack_selected_products"));
   handlePrice({ prod, value, isRemoving: true });
-  const updatedProducts = currentProducts.filter((item) => !(item.prod.id === prod.id && item.value?.id === value?.id));
-  localStorage.setItem("stack_selected_products", JSON.stringify(updatedProducts));
+  const indexToRemove = currentProducts.findIndex((item) => item.prod.id === prod.id && item.value?.id === value?.id);
+  if (indexToRemove !== -1) {
+    currentProducts.splice(indexToRemove, 1);
+    localStorage.setItem("stack_selected_products", JSON.stringify(currentProducts));
+  }
 };
 
 export default handleProductRemoved;
