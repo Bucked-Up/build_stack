e9f5088b460474abb06b60631aebb468df1779e0
deigo-lps <diego.lopes0909@gmail.com>
2025-04-22 17:31:50 -0300

    refactors

--- scripts/handlePrice.js
+++ scripts/handlePrice.js
@@ -15,22 +15,28 @@ const handlePrice = async ({ prod, value, isRemoving }) => {
   const newPrice = isRemoving ? round(currentPrice - price) : round(currentPrice + price);
   currentValueDom.innerHTML = `$${newPrice}`;
   let newWidth = (newPrice / 100) * 100;
-  if (newWidth > 100) newWidth = 100;
-  else if (newWidth <= 0) newWidth = 0;
-  else newWidth -= 10;
-  if (newPrice > 0) leftToUnlockText.classList.add("stack--active");
-  else leftToUnlockText.classList.remove("stack--active");
-  const valueLeft = round(100 - newPrice);
-  if (newPrice < 100) leftToUnlockText.innerHTML = `There is $${valueLeft} left to unlock the stack and activate the discount.`;
-  else if (newPrice > 100) {
-    discountValue = round((20 / 100) * (newPrice - 100));
-    leftToUnlockText.innerHTML = `<span>Stack unlocked.</span> You saved $${round((20 / 100) * (newPrice - 100))} with 20% off overage.`;
-  } else discountValue = 0;
-  range.style.width = `${newWidth}%`;
+  newWidth = newWidth > 100 ? 100 : newWidth <= 0 ? 0 : newWidth - 10;
 
+  if (newPrice > 0) {
+    leftToUnlockText.classList.add("stack--active");
+  } else {
+    leftToUnlockText.classList.remove("stack--active");
+  }
+
+  const valueLeft = round(100 - newPrice);
+  if (newPrice < 100) {
+    leftToUnlockText.innerHTML = `There is $${valueLeft} left to unlock the stack and activate the discount.`;
+  } else {
+    discountValue = newPrice > 100 ? round((20 / 100) * (newPrice - 100)) : 0;
+    leftToUnlockText.innerHTML = newPrice > 100
+      ? `<span>Stack unlocked.</span> You saved $${discountValue} with 20% off overage.`
+      : leftToUnlockText.innerHTML;
+  }
+
+  range.style.width = `${newWidth}%`;
   subtotal.innerHTML = `$${newPrice}`;
   discount.innerHTML = `$${discountValue}`;
-  total.innerHTML = `$${round(newPrice - discountValue)}`
+  total.innerHTML = `$${round(newPrice - discountValue)}`;
 };
 
 export default handlePrice;
