47f33db2833d57a012d9cd849f98dc93e51b28d5
deigo-lps <diego.lopes0909@gmail.com>
2025-04-22 17:29:57 -0300

    adds pricing table

--- scripts/handlePrice.js
+++ scripts/handlePrice.js
@@ -4,6 +4,12 @@ const handlePrice = async ({ prod, value, isRemoving }) => {
   const currentValueDom = document.querySelector("[current-value]");
   const range = document.querySelector(".stack--price-range-wrapper__range span");
   const leftToUnlockText = document.querySelector(".stack--left-to-unlock");
+
+  const subtotal = document.querySelector("[subtotal]");
+  const discount = document.querySelector("[discount]");
+  const total = document.querySelector("[total]");
+  let discountValue = 0;
+
   const price = round(parseFloat(prod.price.split("$")[1]) + parseFloat(value?.price?.split("$")[1] || 0));
   const currentPrice = parseFloat(currentValueDom.innerHTML.split("$")[1]);
   const newPrice = isRemoving ? round(currentPrice - price) : round(currentPrice + price);
@@ -12,14 +18,19 @@ const handlePrice = async ({ prod, value, isRemoving }) => {
   if (newWidth > 100) newWidth = 100;
   else if (newWidth <= 0) newWidth = 0;
   else newWidth -= 10;
-  if(newPrice > 0) leftToUnlockText.classList.add("stack--active")
-  else leftToUnlockText.classList.remove("stack--active")
-  const valueLeft = round(100 - newPrice)
-  if(newPrice < 100) leftToUnlockText.innerHTML = `There is $${valueLeft} left to unlock the stack and activate the discount.`
-  else if(newPrice > 100){
-    leftToUnlockText.innerHTML = `<span>Stack unlocked.</span> You saved $${round(20/100 * (newPrice - 100))} with 20% off overage.`
-  }
+  if (newPrice > 0) leftToUnlockText.classList.add("stack--active");
+  else leftToUnlockText.classList.remove("stack--active");
+  const valueLeft = round(100 - newPrice);
+  if (newPrice < 100) leftToUnlockText.innerHTML = `There is $${valueLeft} left to unlock the stack and activate the discount.`;
+  else if (newPrice > 100) {
+    discountValue = round((20 / 100) * (newPrice - 100));
+    leftToUnlockText.innerHTML = `<span>Stack unlocked.</span> You saved $${round((20 / 100) * (newPrice - 100))} with 20% off overage.`;
+  } else discountValue = 0;
   range.style.width = `${newWidth}%`;
+
+  subtotal.innerHTML = `$${newPrice}`;
+  discount.innerHTML = `$${discountValue}`;
+  total.innerHTML = `$${round(newPrice - discountValue)}`
 };
 
 export default handlePrice;
