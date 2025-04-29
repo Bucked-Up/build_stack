7bdbbd0ca2f356585da51430bfe37603a23b82c9
deigo-lps <diego.lopes0909@gmail.com>
2025-04-29 10:47:01 -0300

    adds prices

--- scripts/dom/createCardBase.js
+++ scripts/dom/createCardBase.js
@@ -1,3 +1,6 @@
+const round = (num) => Math.round(num * 100) / 100;
+const getProdPrice = (prod, value) => round(parseFloat(prod.price.split("$")[1]) + parseFloat(value?.price?.split("$")[1] || 0));
+
 const createCardBase = ({ prod, value, hasFlexDiv }) => {
   const card = document.createElement("div");
   const image = document.createElement("img");
@@ -23,8 +26,21 @@ const createCardBase = ({ prod, value, hasFlexDiv }) => {
     if (value) div.appendChild(flavor);
     card.appendChild(flexWrapper);
   } else {
+    const priceDOM = document.createElement("p");
+    const newPriceDOM = document.createElement("p");
+    const price = getProdPrice(prod, value);
+    const newPrice = round(price - 0.2 * price).toFixed(2);
+    const pricesWrapper = document.createElement("div");
+    pricesWrapper.classList.add("stack--product-card__prices-wrapper");
+    priceDOM.classList.add("stack--product-card__prices-wrapper__old");
+    newPriceDOM.classList.add("stack--product-card__prices-wrapper__new");
+    priceDOM.innerHTML = price.toFixed(2);
+    newPriceDOM.innerHTML = newPrice;
+    pricesWrapper.appendChild(newPriceDOM);
+    pricesWrapper.appendChild(priceDOM);
     card.appendChild(title);
     if (value) card.appendChild(flavor);
+    card.appendChild(pricesWrapper);
   }
   return card;
 };
